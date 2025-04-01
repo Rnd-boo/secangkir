b2ba2ff4e2850795d3e5f78f7cf84f8320f2146e
Randy <rand4035@gmail.com>
2025-04-01 15:32:25 +0700

    create web secangkir

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main>
+      <div className="w-full mx-auto h-20 bg-black">
+        <h1 className="text-3xl font-bold bg-red-300 text-center">
+          Hello world!
+        </h1>
+      </div>
+    </main>
+  );
+}
+export default App;
